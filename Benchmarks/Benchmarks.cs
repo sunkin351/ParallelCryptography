843971c76dd7eb9ccc92d010b33eaa27d3949bfc
Unknown <17joshuanewcomb@gmail.com>
2019-10-10 14:42:13 -0700

    Organized benchmark output, more info

--- Benchmarks/Benchmarks.cs
+++ Benchmarks/Benchmarks.cs
@@ -2,19 +2,18 @@
 using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Configs;
 
 namespace ParallelCryptography.Benchmarks
 {
     [CoreJob]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
     public class HashAlgorithmBenchmarks
     {
         MD5 NativeMD5;
         SHA1 NativeSHA1;
         SHA256 NativeSHA256;
         SHA512 NativeSHA512;
-
-        SHA1Managed Sha1;
-        SHA256Managed Sha256;
 
         [GlobalSetup]
         public void Init()
@@ -23,113 +22,111 @@ namespace ParallelCryptography.Benchmarks
             NativeSHA1 = SHA1.Create();
             NativeSHA256 = SHA256.Create();
             NativeSHA512 = SHA512.Create();
-            Sha1 = new SHA1Managed();
-            Sha256 = new SHA256Managed();
         }
 
-        [Benchmark]
+        [BenchmarkCategory("MD5"), Benchmark(Baseline = true)]
         public byte[] Native_MD5_SingleHash_EmptyInput()
         {
             return NativeMD5.ComputeHash(Array.Empty<byte>());
         }
 
-        [Benchmark]
+        [BenchmarkCategory("MD5"), Benchmark]
         public byte[] MD5_SingleHash_EmptyInput()
         {
             return HashFunctions.MD5(null);
         }
 
-        [Benchmark(OperationsPerInvoke = 4)]
+        [BenchmarkCategory("MD5"), Benchmark(OperationsPerInvoke = 4)]
         public byte[][] MD5_MultiHash_EmptyInput()
         {
             return HashFunctions.MD5Parallel(null, null, null, null);
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA1"), Benchmark(Baseline = true)]
         public byte[] Native_SHA1_SingleHash_EmptyInput()
         {
             return NativeSHA1.ComputeHash(Array.Empty<byte>());
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA1"), Benchmark]
         public byte[] SHA1_SingleHash_EmptyInput()
         {
             return HashFunctions.SHA1(null);
         }
 
-        [Benchmark(OperationsPerInvoke = 4)]
+        [BenchmarkCategory("SHA1"), Benchmark(OperationsPerInvoke = 4)]
         public byte[][] SHA1_MultiHash_EmptyInput()
         {
             return HashFunctions.SHA1Parallel(null, null, null, null);
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA256"), Benchmark(Baseline = true)]
         public byte[] Native_SHA256_SingleHash_EmptyInput()
         {
             return NativeSHA256.ComputeHash(Array.Empty<byte>());
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA256"), Benchmark]
         public byte[] SHA256_SingleHash_EmptyInput()
         {
             return HashFunctions.SHA256(null);
         }
 
-        [Benchmark(OperationsPerInvoke = 4)]
+        [BenchmarkCategory("SHA256"), Benchmark(OperationsPerInvoke = 4)]
         public byte[][] SHA256_MultiHash_EmptyInput()
         {
             return HashFunctions.SHA256Parallel(null, null, null, null);
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA224"), Benchmark(Baseline = true)]
         public byte[] SHA224_SingleHash_EmptyInput()
         {
             return HashFunctions.SHA224(null);
         }
 
-        [Benchmark(OperationsPerInvoke = 4)]
+        [BenchmarkCategory("SHA224"), Benchmark(OperationsPerInvoke = 4)]
         public byte[][] SHA224_MultiHash_EmptyInput()
         {
             return HashFunctions.SHA224Parallel(null, null, null, null);
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA512"), Benchmark(Baseline = true)]
         public byte[] Native_SHA512_SingleHash_EmptyInput()
         {
             return NativeSHA512.ComputeHash(Array.Empty<byte>());
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA512"), Benchmark]
         public byte[] SHA512_SingleHash_EmptyInput()
         {
             return HashFunctions.SHA512(null);
         }
 
-        [Benchmark(OperationsPerInvoke = 2)]
+        [BenchmarkCategory("SHA512"), Benchmark(OperationsPerInvoke = 2)]
         public byte[][] SHA512_MultiHash_2_EmptyInput()
         {
             return HashFunctions.SHA512Parallel(null, null);
         }
 
-        [Benchmark(OperationsPerInvoke = 4)]
+        [BenchmarkCategory("SHA512"), Benchmark(OperationsPerInvoke = 4)]
         public byte[][] SHA512_MultiHash_4_EmptyInput()
         {
             return HashFunctions.SHA512Parallel(null, null, null, null);
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SHA384"), Benchmark(Baseline = true)]
         public byte[] SHA384_SingleHash_EmptyInput()
         {
             return HashFunctions.SHA384(null);
         }
 
-        [Benchmark(OperationsPerInvoke = 2)]
+        [BenchmarkCategory("SHA384"), Benchmark(OperationsPerInvoke = 2)]
         public byte[][] SHA384_MultiHash_2_EmptyInput()
         {
             return HashFunctions.SHA384Parallel(null, null);
         }
 
-        [Benchmark(OperationsPerInvoke = 4)]
+        [BenchmarkCategory("SHA384"), Benchmark(OperationsPerInvoke = 4)]
         public byte[][] SHA384_MultiHash_4_EmptyInput()
         {
             return HashFunctions.SHA384Parallel(null, null, null, null);
